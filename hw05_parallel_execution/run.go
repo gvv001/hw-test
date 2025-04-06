2bebfcb34924c34e08be186ddd5b00a50c36567a
victor <str259@mail.ru>
2025-04-06 22:56:59 +0700

    HW5 is completed

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,6 +2,8 @@ package hw05parallelexecution
 
 import (
 	"errors"
+	"sync"
+	"sync/atomic"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
@@ -10,6 +12,40 @@ type Task func() error
 
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
-	// Place your code here.
+	var errorsCount int32
+	var wg sync.WaitGroup
+	taskChan := make(chan Task)
+
+	worker := func() {
+		defer wg.Done()
+		for task := range taskChan {
+			if err := task(); err != nil {
+				atomic.AddInt32(&errorsCount, 1)
+			}
+		}
+	}
+
+	// Запускаем воркеры
+	for i := 0; i < n; i++ {
+		wg.Add(1)
+		go worker()
+	}
+
+	// передаём таски воркерам если не привысили лимит по ошибкам
+	for _, task := range tasks {
+		if atomic.LoadInt32(&errorsCount) >= int32(m) {
+			break
+		}
+		taskChan <- task
+	}
+
+	close(taskChan)
+	wg.Wait()
+
+	// Если счётчик ошибок больше M, возвращаем ErrErrorsLimitExceeded
+	if atomic.LoadInt32(&errorsCount) >= int32(m) {
+		return ErrErrorsLimitExceeded
+	}
+
 	return nil
 }
--- hw05_parallel_execution/run_test.go
+++ hw05_parallel_execution/run_test.go
@@ -67,4 +67,14 @@ func TestRun(t *testing.T) {
 		require.Equal(t, runTasksCount, int32(tasksCount), "not all tasks were completed")
 		require.LessOrEqual(t, int64(elapsedTime), int64(sumTime/2), "tasks were run sequentially?")
 	})
+	t.Run("Error if M (maxErrorsCount) = 0", func(t *testing.T) {
+		tasksCount := 50
+		tasks := make([]Task, 0, tasksCount)
+
+		workersCount := 10
+		maxErrorsCount := 0
+		err := Run(tasks, workersCount, maxErrorsCount)
+
+		require.Truef(t, errors.Is(err, ErrErrorsLimitExceeded), "actual err - %v", err)
+	})
 }
