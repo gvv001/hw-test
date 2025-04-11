7c5f8ec2476036c3d882ba5c06e3461f06c4e3f8
victor <str259@mail.ru>
2025-04-11 23:44:01 +0700

    HW7 is completed

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -4,33 +4,32 @@ import (
 	"errors"
 	"fmt"
 	"io"
-	"log"
 	"os"
-	"time"
 )
 
 var (
 	ErrUnsupportedFile       = errors.New("unsupported file")
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
-
 	// добавил ошибки.
-	ErrDirectoryIsNotIndicated = errors.New("the directory is not indicated")
-	ErrDirectoryIsNotExist     = errors.New("directory \\tmp is not exist")
+	ErrFromPathNotSpecified = errors.New("'fromPath' not specified")
+	ErrToPathNotSpecified   = errors.New("'toPath' not specified")
 
 	bytesLen, wrritenBytes, sumWrritenBytes int64
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
 	// проверяем указаны ли директории
-	if fromPath == "" || toPath == "" {
-		return ErrDirectoryIsNotIndicated
+	if fromPath == "" {
+		return ErrFromPathNotSpecified
+	}
+	if toPath == "" {
+		return ErrToPathNotSpecified
 	}
 
 	// получаем fileInfo без отрытия файла
 	fileInfo, err := os.Stat(fromPath)
 	if err != nil {
-		fmt.Println(err)
-		return ErrUnsupportedFile
+		return err
 	}
 
 	fileSize := fileInfo.Size()
@@ -44,17 +43,12 @@ func Copy(fromPath, toPath string, offset, limit int64) error {
 
 	readFile, err := os.Open(from)
 	if err != nil {
-		log.Fatal(err, from)
-	}
-
-	err = os.Mkdir("tmp", 0o750)
-	if err != nil && !os.IsExist(err) {
-		log.Fatal(ErrDirectoryIsNotExist)
+		return err
 	}
 
 	outFile, err := os.Create(toPath)
 	if err != nil {
-		log.Fatal(err, from)
+		return err
 	}
 
 	if limit > fileSize-offset {
@@ -62,26 +56,22 @@ func Copy(fromPath, toPath string, offset, limit int64) error {
 	}
 
 	// копируем по 10% байт от limit
-	bytesLen = limit/10
+	bytesLen = limit / 10
 
 	for sumWrritenBytes < limit {
 		readFile.Seek(offset, 0)
 
 		if wrritenBytes, err = io.CopyN(outFile, readFile, bytesLen); err != nil {
 			if !errors.Is(err, io.EOF) {
-				log.Fatal(err)
-				break
+				return err
 			}
 		}
-		
+
 		sumWrritenBytes += wrritenBytes
 		offset += wrritenBytes
 
-		
 		// обновляем прогресс бар
 		showProgress(limit, sumWrritenBytes)
-
-		time.Sleep(time.Millisecond * 20)
 	}
 
 	return nil
